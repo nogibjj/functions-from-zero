47cde67e0c5615b880295468a06613e2adc67821
Noah Gift <noah.gift@gmail.com>
2022-10-04 12:46:18 +0000

    finalizing statements using copilot:

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
-x = 1
-y = 2
-# import ipdb; ipdb.set_trace()
-print(x + y)
+# create a list with different types of fruit
+fruit = ["Apples", "Pears", "Oranges", "Peaches"]
+
+# loop through the fruits and count the total fruits in the list
+for i in fruit:
+    # use an f-string to print the fruit name and the number of the fruit in the list
+    print(f"{i} is fruit number {fruit.index(i)+1}")
+
+# the total number of fruits in the list
+print(f"There are {len(fruit)} fruits in the list")
