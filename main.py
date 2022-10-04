c197ab64ec816e97004cb254c6c3469c59672674
Noah Gift <noah.gift@gmail.com>
2022-10-04 14:36:45 +0000

    adding logic for tests for fastapi

---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+from mylib.logistics import (
+    distance_between_two_points,
+    cities_list,
+    get_coordinates,
+    travel_time
+)
+
+
+app = FastAPI()
+
+
+class City(BaseModel):
+    name: str
+
+
+@app.get("/")
+async def root():
+    """Home Page with GET HTTP Method"""
+
+    return {"message": "Hello Logistics INC"}
+
+
+@app.get("/cities")
+async def cities():
+    """List cities with GET HTTP Method
+
+    Returns back a list of cities that are available to get further information about
+    """
+
+    return {"cities": cities_list()}
+
+#build a post method to calculate the travel time between two cities by car
+@app.post("/travel")
+async def travel(city1: City, city2: City):
+    """Estimate travel time between two cities by car with POST HTTP Method
+
+    Returns back the travel time between two cities by car.
+    FIXME: Bug
+    """
+
+    hours = travel_time(city1, city2)
+    return {"travel_time": f"{hours} hours"}
+    
+@app.post("/distance")
+async def distance(city1: City, city2: City):
+    """Calculate distance between two cities with POST HTTP Method
+
+    Returns back the distance between two cities in miles
+    """
+
+    return {
+        "distance": distance_between_two_points(
+            get_coordinates(city1.name), get_coordinates(city2.name)
+        )
+    }
+
+
+if __name__ == "__main__":
+
+    uvicorn.run(app, port=8080, host="0.0.0.0")
--- test_logistics.py
+++ test_logistics.py
@@ -1,9 +1,21 @@
-from mylib.logistics import CITIES, distance_between_two_points, print_cities
-
+from mylib.logistics import CITIES, distance_between_two_points, cities_list
+from fastapi.testclient import TestClient
+from main import app
+import pytest
 
 def test_distance_between_two_points():
     assert distance_between_two_points(CITIES[0][1], CITIES[1][1]) == 2450.9503446683375
 
 
-def test_print_cities():
-    assert "Dallas" in print_cities()
+def test_cities_list():
+    assert "Dallas" in cities_list()
+
+@pytest.fixture
+def client():
+    with TestClient(app) as client:
+        yield client
+
+def test_read_main(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello Logistics INC"}
